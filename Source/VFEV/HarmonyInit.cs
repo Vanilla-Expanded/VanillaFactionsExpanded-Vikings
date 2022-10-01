67241191d3ca86c71a3f3dd9b3eca23d575d0eed
KylianB <kylian.balde@gmail.com>
2022-10-01 15:09:28 +0200

    harmony patch fix

--- Source/VFEV/HarmonyInit.cs
+++ Source/VFEV/HarmonyInit.cs
@@ -1,17 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using HarmonyLib;
+﻿using HarmonyLib;
 using RimWorld;
-using UnityEngine;
 using Verse;
-using Verse.AI;
 
 namespace VFEV
 {
 
     [StaticConstructorOnStartup]
-    internal static class HarmonyInit
+    public static class HarmonyInit
     {
         static HarmonyInit()
         {
@@ -20,26 +15,26 @@ namespace VFEV
     }
 
     [HarmonyPatch(typeof(Toils_LayDown))]
-    class PatchToils_LayDown
+    [HarmonyPatch("ApplyBedThoughts", MethodType.Normal)]
+    public class PatchToils_LayDown
     {
         [HarmonyPostfix]
-        [HarmonyPatch("ApplyBedThoughts")]
-        [HarmonyPatch(new Type[] { typeof(Pawn) })]
         static void PostFix(Pawn actor)
         {
-            Building_Bed building_Bed = actor.CurrentBed();
-            if (building_Bed != null && building_Bed.def.defName.Contains("FurBed")) actor.needs.mood.thoughts.memories.RemoveMemoriesOfDef(ThoughtDefOf.SleptInCold);
+            if (actor.CurrentBed() is Building_Bed bed && (bed.def.defName == "VFEV_FurBed" || bed.def.defName == "VFEV_DoubleFurBed"))
+                actor.needs.mood.thoughts.memories.RemoveMemoriesOfDef(ThoughtDefOf.SleptInCold);
         }
     }
 
     [HarmonyPatch(typeof(RaceProperties))]
-    class PatchIsMechanoid
+    [HarmonyPatch("IsMechanoid", MethodType.Getter)]
+    public class PatchIsMechanoid
     {
         [HarmonyPostfix]
-        [HarmonyPatch("IsMechanoid", MethodType.Getter)]
         static void PostFix(ref RaceProperties __instance, ref bool __result)
         {
-            if (__instance.FleshType == FleshTypeDefOf.Mechanoid && __instance?.body.defName == "VFEV_Odin") __result = false;
+            if (__instance.FleshType == FleshTypeDefOf.Mechanoid && __instance?.body.defName == "VFEV_Odin")
+                __result = false;
         }
     }
 }
